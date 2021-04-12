96f7c048ac34909bec95f97ebf6d545b4955f8d4
Abhishek Mohanty <abhishekm2106@gmail.com>
2021-04-12 13:58:18 +0530

    update shop data with new data and updated add items to cart to update quantity

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -3,3 +3,8 @@ import cartActiontypes from './cart.types'
 export const toggleCartHidden = () => ({
     type: cartActiontypes.TOGGLE_CART_HIDDEN
 })
+
+export const addItem = item =>({
+    type: cartActiontypes.ADD_ITEM,
+    payload: item
+})
